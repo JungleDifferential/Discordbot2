a8a86c71069264f3b42a728726536f39e2b58dbe
Darius <dduciuc@uw.edu>
2022-08-01 14:22:57 -0700

    tidied up voiceStateUpdate, greets specific users

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
-const { Client, Collection, Intents, MessageSelectMenu } = require("discord.js");
+const { Client, Collection, Intents } = require("discord.js");
 const { token } = require("./config.json");
-const { generateDependencyReport, joinVoiceChannel, getVoiceConnection } = require("@discordjs/voice");
+const { generateDependencyReport, joinVoiceChannel, getVoiceConnection, createAudioPlayer, createAudioResource } = require("@discordjs/voice");
 
 // setup the client and the external (idk if thats the right word) commands
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
@@ -12,14 +12,26 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
+// audio player
+const player = createAudioPlayer();
+
+let currentChannel = null;
+
+// IDs of specific important members
+const bigB = "392518224589225985";
+const bigC = "364090299519336448";
+const bigN = "317117364623900673";
+
 // The Ids of the "default" and mute/deafen channels
 const stdChnl = "692568922360119326";
 const muteChnl = "692196640349487175";
+
 // the state of the bot; disabled, mute, or deafen
-var crime = "disabled";
+let crime = "disabled";
+
 // maps user Ids to the channel Id that they were in before they were moved to the mute/deffen channel.
 // used for moving them back to where they were
-var memberChnls = new Map();
+let memberChnls = new Map();
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
@@ -74,8 +86,11 @@ client.on("messageCreate", async message => {
 		joinVoiceChannel({
 			channelId: message.member.voice.channel.id,
 			guildId: message.guild.id,
+			selfDeaf: false,
 			adapterCreator: message.guild.voiceAdapterCreator
 		})
+		currentChannel = message.member.voice.channel.id;
+		console.log(currentChannel);
 		return;
 	}
 
@@ -87,7 +102,20 @@ client.on("messageCreate", async message => {
 			return;
 		}
 		connection.destroy();
+		currentChannel = null;
 		return;
+	}
+
+	if (command == "play") {
+		const connection = getVoiceConnection(message.guild.id);
+
+		if (!connection) {
+			await message.reply("Dawg I'm not in a voice channel🤓")
+			return;
+		}
+		player.play(createAudioResource(`audio/Whats_up_Big_C.wav`));
+		connection.subscribe(player);
+		return
 	}
 
 });
@@ -95,25 +123,50 @@ client.on("messageCreate", async message => {
 // when someone updates a their voicestate (join/leave chnl, mute/unmute/deafen/undeafen)
 // it will move them to mute/deffeners if they violated the "crime"
 client.on("voiceStateUpdate", async (oldState, newState) => {
-	let chnl = null;
-	if (newState.channel != null) {
-		chnl = newState.channel.name;
+	let channelId = newState.channelId;
+	if (channelId == null) {
+		return;
 	}
-	if (chnl != null) {
-		if (chnl != "Mute/Deffeners" && ((crime == "mute" && newState.selfMute) || (crime == "deafen" && newState.selfDeaf))) {
-			memberChnls.set(newState.member.id, newState.channelId);
-			newState.setChannel(client.channels.cache.get(muteChnl), "if you are muted/deafened, you belong in Mute/Deffeners!! OwO");
+
+	// dealing with moving people who are/arent muted/deafened
+	if (channelId == muteChnl && ((crime == "mute" && !newState.selfMute) || (crime == "deafen" && !newState.selfDeaf))) {
+		if (memberChnls.has(newState.member.id)) {
+			newState.setChannel(client.channels.cache.get(memberChnls.get(newState.member.id)));
+			memberChnls.delete(newState.member.id);
+			return;
 		}
-		if (chnl == "Mute/Deffeners" && ((crime == "mute" && !newState.selfMute) || (crime == "deafen" && !newState.selfDeaf))) {
-			if (memberChnls.has(newState.member.id)) {
-				newState.setChannel(client.channels.cache.get(memberChnls.get(newState.member.id)));
-				memberChnls.delete(newState.member.id);
-				return;
-			}
-			newState.setChannel(client.channels.cache.get(stdChnl), "non deffeners dont belong there");
+		newState.setChannel(client.channels.cache.get(stdChnl), "non deffeners dont belong there");
+		return;
+	}
+	if ((crime == "mute" && newState.selfMute) || (crime == "deafen" && newState.selfDeaf)) {
+		memberChnls.set(newState.member.id, channelId);
+		newState.setChannel(client.channels.cache.get(muteChnl), "if you are muted/deafened, you belong in Mute/Deffeners!! OwO");
+		return;
+	}
+	
+	// when specific individuals join, and mute/deffeners is in the call, greet them
+	if (currentChannel != null && oldState.channelId != currentChannel && channelId == currentChannel) {
+		const connection = getVoiceConnection(newState.guild.id);
+		switch (newState.member.id) {
+			case bigB:
+				player.play(createAudioResource(`audio/whats up big b.wav`));
+				connection.subscribe(player);
+			case bigC:
+				player.play(createAudioResource(`audio/whats up big c.wav`));
+				connection.subscribe(player);
+			case bigN:
+				player.play(createAudioResource(`audio/sup sup big n.wav`));
+				connection.subscribe(player);
 		}
+		
 	}
 });
+
+// to deal with when the player has an error
+player.on("error", error => {
+	console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
+	player.play(getNextResource());
+})
 
 // Login to Discord with your client's token (useless comment tbh)
 console.log(token);
